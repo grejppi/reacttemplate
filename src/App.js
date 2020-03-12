aea41904b8e673e0517ad77c5591fd2df8c3ecd3
grejppi <grejppi@users.noreply.github.com>
2020-03-12 10:07:04 +0200

    If you're reading this, the web server was installed correctly.™

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './Components/Header';
+
+const App = () => {
+  return (
+    <Header
+      title={ "CompanyName.website" }
+      subtitle={ "If you're reading this, the web server was installed correctly.\u2122" }
+    />
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+const Header = ({ title, subtitle }) => {
+  useEffect(() => document.title = title);
+
+  return (
+    <header>
+      <h1>{ title }</h1>
+      <h2>{ subtitle }</h2>
+    </header>
+  );
+};
+
+export default Header;
